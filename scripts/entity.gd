911c1f11f79d17dc562eec34201b682daad3f6ce
AnigamerWasTakenTwice <mohammed.alqarooni@gmail.com>
2026-07-22 19:45:24 -0400

    implemented tool usage

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 @export var health = 10
 @export var drop: String
+@export var type: String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Hitbox.set_meta("type", type)
 	pass # Replace with function body.
 
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,17 @@ extends CharacterBody2D
 
 @export var movement_speed: float
 @export var movement_max_speed: float
+@export var run_accel_multiplier: float
+@export var run_max_speed_multiplier: float
 var movement_direction: Vector2
 @onready var interaction_area: Area2D = $InteractionArea
 
+var tools = [
+	"axe",
+	"pickaxe"
+]
+
+var selected_tool = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,19 +22,23 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	movement_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if Input.is_action_pressed("run"):
-		velocity.x = clamp(velocity.x + movement_direction.normalized().x * movement_speed * 60 * delta * 3, -movement_max_speed * 1.5, movement_max_speed * 1.5)
-		velocity.y = clamp(velocity.y + movement_direction.normalized().y * movement_speed * 60 * delta * 3, -movement_max_speed * 1.5, movement_max_speed * 1.5)
+		velocity.x = clamp(velocity.x + movement_direction.normalized().x * movement_speed * 60 * delta * run_accel_multiplier, -movement_max_speed * run_max_speed_multiplier, movement_max_speed * run_max_speed_multiplier)
+		velocity.y = clamp(velocity.y + movement_direction.normalized().y * movement_speed * 60 * delta * run_accel_multiplier, -movement_max_speed * run_max_speed_multiplier, movement_max_speed * run_max_speed_multiplier)
 	else:
 		velocity.x = clamp(velocity.x + movement_direction.normalized().x * movement_speed * 60 * delta, -movement_max_speed, movement_max_speed)
 		velocity.y = clamp(velocity.y + movement_direction.normalized().y * movement_speed * 60 * delta, -movement_max_speed, movement_max_speed)
+	
 	velocity = lerp(velocity, Vector2.ZERO, 0.2)
 	move_and_slide()
 	if movement_direction != Vector2.ZERO: 
 		interaction_area.position = Vector2(0, -64) + (movement_direction * 96)
-	if Input.is_action_just_pressed("attack"):
-		interact("attack")
-	if Input.is_action_just_pressed("interact"):
-		interact("interact")
+	if Input.is_action_just_pressed("attack"): interact("attack")
+	if Input.is_action_just_pressed("interact"): interact("interact")
+	if Input.is_action_just_pressed("tool"): interact(tools[selected_tool])
+	if Input.is_action_just_pressed("cycle"):
+		if selected_tool < tools.size() - 1: selected_tool += 1
+		else: selected_tool = 0
+		$UI/ToolSelected.text = tools[selected_tool]
 	pass
 
 func interact(type: String):
